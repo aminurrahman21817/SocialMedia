6ec5bd21f19c422c67728c8f34b433e366d5bf83
aminurrahman21817 <git config --global user.name Your>
2019-03-30 17:15:03 +0100

    style

---
+++ assets/js/register.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#signup").click(function(){
+    $("#first").slideUp("slow",function(){
+    	$("#second").slideDown("slow");
+    });
+  });
+});
+
+
+
+$(document).ready(function(){
+  $("#signin").click(function(){
+    $("#second").slideUp("slow",function(){
+    	$("#first").slideDown("slow");
+    });
+  });
+});
